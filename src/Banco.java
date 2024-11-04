d2fc419c62a0875811abf2fbb9b6011c5ddaaad9
higorgio <higorgiovane@ucashapp.com>
2024-11-04 15:56:41 -0300

    up

---
+++ src/Banco.java
@@ -0,0 +1,31 @@
+// Banco.java
+class Banco {
+    public static void main(String args[]) {
+        Conta c1;
+        c1 = new ContaNormal();
+        c1.setNumero("1654-3");
+        c1.setSaldo(500);
+
+        ContaDebEspecial c2 = new ContaDebEspecial();
+        ContaDebEspecial c3 = new ContaDebEspecial("4067-6", 2500, 5050);
+        c2.setNumero("4067-6");
+        c2.setSaldo(2500);
+        c2.setLimite(1000.67);
+
+        System.out.println("A conta número " + c1.getNumero() + " possui saldo " + c1.getSaldo());
+        c1.creditar(1000);
+        System.out.println("Após o crédito de R$ 1000,00, a conta número " + c1.getNumero() + " passou a ter saldo " + c1.getSaldo());
+
+        c1.debitar(100);
+        System.out.println("Após o débito de R$ 100,00, a conta número " + c1.getNumero() + " passou a ter saldo " + c1.getSaldo());
+
+        System.out.println("\nA conta número " + c2.getNumero() + " possui saldo " + c2.getSaldo());
+        c2.debitar(500);
+        System.out.println("Após o débito de R$ 500,00, a conta número " + c2.getNumero() + " possui saldo " + c2.getSaldo());
+
+        System.out.println("A conta número " + c2.getNumero() + " possui saldo " + c2.getSaldo() + " e limite de " + c2.getLimite());
+
+        c2.setLimite(10000);
+        System.out.println("A conta número " + c2.getNumero() + " possui saldo " + c2.getSaldo() + " e novo limite de " + c2.getLimite());
+    }
+}
---
+++ src/ContaNormal.java
@@ -0,0 +1,13 @@
+// ContaNormal.java
+public class ContaNormal extends Conta {
+
+ public void creditar (double valor) {
+ saldo = saldo + valor;
+ }
+
+ public void debitar (double valor) {
+ if ((saldo-valor) >= 0) {
+ saldo = saldo - valor;
+ }
+ }
+}
